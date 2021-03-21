43de12a8720165bc2519e363b41966d6444d47e6
stacktracer <vostroknutov@hotmail.com>
2021-03-21 14:57:28 +0300

    fix: fix name DEVPAPER_TOKEN

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
+const toStr = data => JSON.stringify(data, null, 2);
+
 async function run() {
 
-    const token = core.getInput('GITHUB_TOKEN');
-    const kit = github.getOctokit(token);
+    const DEVPAPER_TOKEN = core.getInput('DEVPAPER_TOKEN');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
-    console.log('Kit init with context', github.context);
+    core.debug(toStr({ DEVPAPER_TOKEN, GITHUB_TOKEN }))
+
+    const kit = github.getOctokit(DEVPAPER_TOKEN);
+
+    core.debug('Context:\n');
+    core.debug(toStr(github.context))
 
 }
 
